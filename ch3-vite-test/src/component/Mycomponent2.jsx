2d6194061b06912aaadf131e2aeb06d7d035524a
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-15 12:44:58 +0900

    250115 1,2,3,4교시 수업 및 미니실습

---
+++ ch3-vite-test/src/component/Mycomponent2.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+//유효성 체크 해보는 도구
+import PropTypes from 'prop-types';
+
+class Mycomponent2 extends Component {
+  render() {
+    const { name, children, favoriteNumber } = this.props;
+    return (
+      <div>
+        넘어온 데이터 1 name : {name}
+        넘어온 데이터 1 children : {children}
+        넘어온 데이터 1 favoriteNumber : {favoriteNumber}
+      </div>
+    );
+  }
+}
+
+// 기본 props 값 설정
+Mycomponent2.defaultProps = {
+  name: '기본 이름',
+};
+
+// 해당 props 타입 지정
+// 두번째, isRequired 설정
+Mycomponent2.propTypes = {
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired,
+};
+
+export default Mycomponent2;
---
+++ ch3-vite-test/src/component/Say2.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const Say2 = () => {
+  const [message, setMessage] = useState('강두현님,환영합니다');
+  const [color, setColor] = useState('black');
+  const onClickEnter = () => setMessage('안녕하세요!');
+  const onClickLeave = () => setMessage('안녕히 가세요!');
+  const welcome = () => setMessage('환영합니다!');
+  const textButton = () => {
+    alert('입력된 메시지: ' + message);
+  };
+  const resetColor = () => setColor('black');
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="메세지를 입력하세요"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        //onChange 이벤트는 사용자가 입력 필드의 내용을 변경할 때마다 발생함.
+        // 즉, 사용자가 텍스트를 입력하거나 수정할 때마다 호출됨.
+        //(e)는 이벤트 객체,이 객체는 input 요소에서 발생한 이벤트에 대한 정보를 담고있음
+        //e.target.value는 입력 필드의 현재 값. 즉, 사용자가 input 필드에 입력한 텍스트 값을 가져오는거.
+        //setMessage(e.target.value)는 message 상태를 입력된 값으로 업데이트하는거
+      />
+
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <button onClick={welcome}>환영합니다</button>
+      <button onClick={textButton}>메시지 출력</button>
+
+      {/* //추가2 */}
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        빨간색
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        초록색
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        파란색
+      </button>
+      <button onClick={resetColor}>글자색 초기화</button>
+    </div>
+  );
+};
+
+export default Say2;
