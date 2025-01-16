ac5f182c0924045cda26d6fb28b51c4430bcb821
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-16 10:28:34 +0900

    250116 1,2교시  전날 미니실습 복습

--- ch4-vite-test/src/App.jsx
+++ ch4-vite-test/src/App.jsx
@@ -1,22 +1,19 @@
 import './App.css';
-// import EventPractice from './component/EventPractice';
-// import EventPractice2 from './component/EventPractice2';
-// import Say2 from './component/Say2';
-// import Login from './component/Login';
-import Login from './component/Login';
-import SignUp from './component/SignUp';
+import EventPractice from './component/EventPractice';
+import EventPractice2 from './component/EventPractice2';
+import JoinForm from './component/JoinForm';
+import LoginForm from './component/LoginForm';
+import Say from './component/Say';
 
 function App() {
   return (
     <>
-      {/* <h1 className="react">ch4 이벤트 핸들러 해보기</h1>
-      {/* <Say2 />
-      <EventPractice />
-      <EventPractice2 /> */}
-      {/* <Login />  */}
-      <h1>로그인 및 회원가입 미니실습</h1>
-      <Login />
-      <SignUp />
+      <h1 className="react">ch4 이벤트 핸들러 해보기</h1>
+      {/* <Say /> */}
+      {/* <EventPractice /> */}
+      {/* <EventPractice2 /> */}
+      {/* <LoginForm /> */}
+      <JoinForm />
     </>
   );
 }
--- ch4-vite-test/src/component/EventPractice2.jsx
+++ ch4-vite-test/src/component/EventPractice2.jsx
@@ -14,6 +14,12 @@ const EventPractice2 = () => {
   // 교체작업1-2
   const { username, message } = form;
 
+  // 실습 1, 출력용 , state 정의.
+  // useState(''); -> 반환을 배열을 반환함. 1번째요소, 초깃값,
+  // 2번째요소, 1번째 값을 설정하는 setter 함수가 반환.
+  // output: 초깃값, setOutput : 함수.
+  const [output, setOutput] = useState('');
+
   // 교체작업1-3
   // const onChangeUsername = (e) => setUsername(e.target.value);
   // const onChangeMessage = (e) => setMessage(e.target.value);
@@ -26,7 +32,7 @@ const EventPractice2 = () => {
   // 교체작업1-4
   const onChange = (e) => {
     const nextForm = {
-      //... << 스프레드 연산자, 기존값을 복사를 함
+      // ... 스프레드 연산자, 기존 값을 복사를 함.
       ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
       [e.target.name]: e.target.value, // 원하는 값을 덮어 씌우기
     };
@@ -34,11 +40,30 @@ const EventPractice2 = () => {
   };
   // 교체작업1-5
   const onClick = () => {
-    alert(username + ': ' + message);
+    // alert(username + ': ' + message);
+
+    // 실습 1-2, 출력용 output 곳에 값을 설정.
+    // 주의사항,
+    // output = 직접 값을 할당 . x
+    // setOutput 함수를 이용해서, 값을 할당. 설정.
+
+    // 실습 2, 유효성 체크. username, message , 값이 없다면, 경고창 띄우기.
+    if (!username || !message) {
+      //!username , 값을 없을 경우 실행하겠다.
+      alert('username 과 message 모두 작성해주세요.!!!');
+    }
+    setOutput(`username : ${username} , message: ${message}`);
     setForm({
       username: '',
       message: '',
     });
+  };
+
+  //실습3, form 의 내용을 초기화를 해요.
+  // form 의 내용을 직접 변경 해야하나요?
+  // 아니면, 세터 함수를 이용해야하나요?
+  const onClear = () => {
+    setForm({ username: '', message: '' });
   };
 
   const onKeyPress = (e) => {
@@ -50,8 +75,10 @@ const EventPractice2 = () => {
   return (
     <div>
       <h1>이벤트 연습, 함수형 컴포넌트 버전.</h1>
-      <h1>message : {message}</h1>
-      <h1>username : {username}</h1>
+      {/* <h1>message : {message}</h1>
+            <h1>username : {username}</h1> */}
+      {/* 실습 1-3 화면 출력해보기 */}
+      <h2>출력용 결과 확인 : {output}</h2>
       <input
         type="text"
         name="username"
@@ -71,6 +98,7 @@ const EventPractice2 = () => {
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
+      <button onClick={onClear}>초기화</button>
     </div>
   );
 };
