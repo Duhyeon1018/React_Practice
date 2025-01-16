880c305a62dfc7819298201476c7e1a64b9ca58d
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-16 12:12:14 +0900

    250116 2,3,4 교시 수업 및 미니실습

---
+++ ch5-vite-test/src/App.jsx
@@ -0,0 +1,37 @@
+// 교체 작업 3
+import React, { Component } from 'react';
+import './App.css';
+import RefSample from './component/RefSample';
+import ValidationSample from './component/ValidationSample';
+import ScrollBox from './component/ScrollBox';
+import ScrollBox2 from './component/ScrollBox2';
+
+// function App() {
+// 함수형 컴포넌트 -> 클래스 형 컴포넌트 변경.
+// 교체 작업 1
+class App extends Component {
+  // 교체 작업 2
+  render() {
+    return (
+      <>
+        <h1 className="react">ch5 ref, DOM 요소에 이름달기</h1>
+        {/* <ValidationSample />
+      <RefSample /> */}
+        {/* 교체 작업 4 */}
+        {/* 부모에서 자식 컴포넌트 방향으로 props 전달하기.  */}
+        {/* 컴포넌트에 접근하기 위해서, 컴포넌트에 ref 달기.  */}
+        <ScrollBox2 ref={(ref) => (this.scrollBox2 = ref)} />
+        {/* 부모에서 자식 컴포넌트의 ref에 접근해서, 자식 컴포넌트 안에 메서드 호출하기 */}
+        <button onClick={() => this.scrollBox2.scrollToBottom()}>
+          맨밑으로
+        </button>
+        <button onClick={() => this.scrollBox2.scrollToTop()}>맨위로</button>
+        <button onClick={() => this.scrollBox2.scrollToMiddle()}>
+          중간으로
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ ch5-vite-test/src/component/ScrollBox2.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class ScrollBox2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollPosition: 0, // 스크롤 위치 상태 추가
+    };
+  }
+
+  // 스크롤 이벤트 핸들러
+  handleScroll = () => {
+    const { scrollTop } = this.box;
+    this.setState({ scrollPosition: scrollTop }); // scrollTop을 상태에 업데이트
+  };
+
+  scrollToBottom = () => {
+    const { scrollHeight, clientHeight } = this.box;
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+
+  scrollToTop = () => {
+    this.box.scrollTop = 0;
+  };
+
+  scrollToMiddle = () => {
+    this.box.scrollTop = 175;
+  };
+
+  render() {
+    const style = {
+      border: '1px solid black',
+      height: '300px',
+      width: '300px',
+      overflow: 'auto',
+      position: 'relative',
+    };
+
+    const innerStyle = {
+      width: '100%',
+      height: '650px',
+      background: 'linear-gradient(pink, red)',
+    };
+
+    return (
+      <div>
+        {/* 스크롤 위치를 박스 바깥에 위치 */}
+        <div style={{ marginBottom: '10px' }}>
+          현재 스크롤 위치: {this.state.scrollPosition}px
+        </div>
+
+        {/* 스크롤 박스 */}
+        <div
+          style={style}
+          ref={(ref) => {
+            this.box = ref;
+          }}
+          onScroll={this.handleScroll} // 스크롤 이벤트 핸들러 추가
+        >
+          <div style={innerStyle} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ScrollBox2;
