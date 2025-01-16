14e14b2da5668d88011f3986e625d9fa408d88c9
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-16 15:21:03 +0900

    250116 5,6교시

---
+++ ch6-vite-test/src/App.jsx
@@ -0,0 +1,13 @@
+import IterationSample from '../component/IterationSample';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="react">ch6 컴포넌트 반복</h1>
+      <IterationSample />
+    </>
+  );
+}
+
+export default App;
