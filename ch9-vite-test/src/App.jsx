96cc5fd6bf3999b20295c5c7809a8567e19b7381
Duhyeon Kang <rkdengus1234@naver.com>
2025-01-20 15:34:03 +0900

    250120 5,6 교시

--- ch9-vite-test/src/App.jsx
+++ ch9-vite-test/src/App.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
 import CSSModule from './components/CSSModule';
+import SassComponent from './components/SassComponent';
+import StyledComponent from './components/styledComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
         {/* <SassComponent /> */}
-        <CSSModule />
+        {/* <CSSModule /> */}
+        <StyledComponent />
       </div>
     );
   }
